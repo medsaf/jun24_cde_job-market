e209cd54b25712251c5dc5e704edcb2bced1e435
Ubuntu <ubuntu@ip-172-31-30-159.eu-west-1.compute.internal>
2025-05-05 16:43:42 +0000

    First_dag_added

--- kubernetes/airflow/dags/dags.py
+++ kubernetes/airflow/dags/dags.py
@@ -2,35 +2,17 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 import random
+from FranceTravailDataExtractor2.py import *
 
-def function_with_return(task_instance):
-    task_instance.xcom_push(
-        key="my_xcom_value",
-        value=random.uniform(a=0, b=1)
-    )
-
-def read_data_from_xcom(task_instance):
-    print(
-        task_instance.xcom_pull(
-            key="my_xcom_value",
-            task_ids=['python_task']
-        )
-    )
 
 with DAG(
-    dag_id='simple_xcom_dag',
+    dag_id='Extract_and_load_data',
     schedule_interval=None,
     start_date=days_ago(0)
 ) as my_dag:
 
-    my_task = PythonOperator(
+    load_and_extract = PythonOperator(
         task_id='python_task',
-        python_callable=function_with_return
+        python_callable=load_and_extract
     )
-
-    my_task2 = PythonOperator(
-        task_id="read_Xcom_value",
-        python_callable=read_data_from_xcom
-    )
-    
-    my_task >> my_task2
+    my_task 
