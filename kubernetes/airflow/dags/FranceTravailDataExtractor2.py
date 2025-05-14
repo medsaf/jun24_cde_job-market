988d586f2bcc82b708328ee987dbcd226b92d535
Ubuntu <ubuntu@ip-172-31-30-159.eu-west-1.compute.internal>
2025-05-14 19:19:09 +0000

    dag to insert data

--- kubernetes/airflow/dags/FranceTravailDataExtractor2.py
+++ kubernetes/airflow/dags/FranceTravailDataExtractor2.py
@@ -1700,7 +1700,7 @@ def main():
             close_connection(cursor, connection)
         
 
-def Extract_and_load_data(OUTPUT_DIR="dags/repo/kubernetes/airflow/dags",CSV_FILE_PATH="kubernetes/airflow/dags/french_departments.csv"):
+def Extract_and_load_data(OUTPUT_DIR="dags/repo/kubernetes/airflow/dags",CSV_FILE_PATH="dags/repo/kubernetes/airflow/dags/french_departments.csv"):
     cursor, connection = establish_connection()
     insert_requirements(cursor, connection)
     load_data_to_db(cursor, connection,OUTPUT_DIR, CSV_FILE_PATH)
